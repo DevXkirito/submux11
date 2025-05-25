9b9b0456f2c2a562597e2bdbb0b27e2727033b5c
DevXkirito <111516421+DevXkirito@users.noreply.github.com>
2025-05-25 13:01:52 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,4 +7,4 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
-FONT_PATH = "assets/fonts/HelveticaRounded-Bold.ttf"
+FONT_PATH = "fonts/HelveticaRounded-Bold.ttf"
